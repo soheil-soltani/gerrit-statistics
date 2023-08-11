a69e3aef79d7c382dc9d4e0a9fbd7d95ecb77463
Soheil Soltani <8424923+soheil-soltani@users.noreply.github.com>
2023-08-11 15:50:17 +0200

    Add docstrings forsrc/ __init__.py

--- gerritstats/src/__init__.py
+++ gerritstats/src/__init__.py
@@ -1,9 +1,36 @@
+"""
+This module provides the API through the GerritStats class.
+
+This module contains the definition of the GerritStats class.
+
+Author: Soheil Soltani
+"""
+
+# Third-party imports
 from gerrit import GerritClient
+# Local imports
 from gerritstats.src.project import GerritProject
 
 
 class GerritStats:
-    def __init__(self,username, password, url):
+    """
+    Implement the API.
+
+    This class implements the outward facing API, which
+    can be used by the user application.
+    """
+
+    def __init__(self, username, password, url):
+        """
+        Constructor
+
+        Args:
+            username (str): Username for authenticating to Gerrit
+            password (str): Password for authenticating to Gerrit
+            url (str): The base URL of the Gerrit repository
+        Returns:
+            None
+        """
         self.uname = username
         self.pw = password
         self.url = url
@@ -11,55 +38,111 @@ class GerritStats:
 
     @property
     def client(self):
+        """
+        Compute property to get the gerrit client
+
+        Returns:
+            A GerritClient object to be used for communicating with
+            the Gerrit API
+        """
         return GerritClient(self.url, self.uname, self.pw)
-        
 
     def set_projects(self, include=None, exclude=None):
+        """
+        Create a list of Gerrit projects
+
+        This method creates a list of Gerrit projects by querying
+        the Gerrit repository represented using the base URL. It
+        then assigns this list to the attribute projects
+        It allows some filtering by only including those projects
+        that contain the include strings. It also excludes from the
+        list all those project that contain any of the exclude
+        strings in the project name.
+
+        Args:
+             include (list): List of strings (optional)
+             exclude (list): List of strings (optional)
+
+        Returns:
+            None
+        """
         projects_dict = self.client.projects.list(limit=None)
-        projects_list = list(projects_dict.keys())  #TODO: set()?
-              
+        projects_list = list(projects_dict.keys())  # TODO: set()?
+
         if include:
             new_projects = [proj for proj in projects_list if
                             any(phrase in proj for phrase in include)]
-                     
             projects_list = new_projects
-              
+
         if exclude:
             new_projects = [proj for proj in projects_list if
                             any(phrase not in proj for phrase in exclude)]
-            
             projects_list = new_projects
 
-            self.projects = projects_list
+        self.projects = projects_list
 
-                     
     def get_projects(self):
+        """
+        Get the list of Gerrit projects
+
+        This method gets the list of Gerrit projects specified
+        using the set_projects() method
+
+        Returns:
+            project (list of strings): List of project names
+        """
         return self.projects
 
-
     def get_project_stats(self, project_id, begin, end, status, branch='master'):
+        """
+
+        """
         project = GerritProject(proj_id=project_id, url=self.url, username=self.uname,
                                 password=self.pw)
-        
+
         min_rate, max_rate, avg_rate = project.get_statistics(begin=begin,
                                                               end=end,
                                                               status=status,
                                                               branch=branch)
         return min_rate, max_rate, avg_rate
 
-
     def get_project_changes(self, project_id, begin, end, status, branch='master'):
-        project = GerritProject(proj_id=project_id, url=self.url, username=self.uname,
-                                password=self.pw)
-         
-        changes = project.get_changes(begin=begin, end=end, status=status, branch=branch)
+        """
+        Retrieve the changes for a specific project
 
-        return changes
+        This method queries the Gerrit API for the changes using the specific
+        project, branch, and the status of the change
 
+        Args:
+            project_id (str): Gerrit Project ID contained in self.project
+            begin (str): Exclude changes older than this date (Format: YY-MM-DD)
+            end (str): Exclude changes newer than this date (Format: YY-MM-DD)
+            status (str): Include changes currently in this status (Format: status:merged, etc.)
+            branch (str): Include changes listed on this branch (optional)
+
+        Returns:
+            list: List of retrieved changes
+            bool: Success flag
+        """
+        project = GerritProject(proj_id=project_id, url=self.url, username=self.uname, password=self.pw)
+        changes, success = project.get_changes(begin=begin, end=end, status=status, branch=branch)
+
+        return changes, success
 
     def get_change(self, project_id, change_id):
-        project = GerritProject(proj_id=project_id, url=self.url, username=self.uname,
-                                password=self.pw)
+        """
+        Get details of a specific change
+
+        This method retrieves the change details using the Gerrit API
+
+        Args:
+            project_id (str): Gerrit Project ID contained in self.project
+            change_id (str): Change ID returned by get_project_changes()
+
+        Returns:
+            dict: A json-formatted dictionary of the change details
+        """
+        project = GerritProject(proj_id=project_id, url=self.url, username=self.uname, password=self.pw)
         change_details = project.get_change_details(change_id=change_id)
 
         return change_details
@@ -73,49 +156,49 @@ class GerritStats:
         return plus_twos
 
 """
-    #def to_be_testes():
-       # These methods are not yet tested
-       # def get_log(client, project, branch='master'):
-       #        """Get git reflog of the given project
-       #        """
-              
-       #        repo=client.projects.get(project)
-              
-       #        try:
-       #               target=repo.branches.get('refs/heads/'+branch)            
-       
-       #        except Exception:
-       #               print('{}'.format(project))
+# def to_be_testes():
+# These methods are not yet tested
+# def get_log(client, project, branch='master'):
+#        """Get git reflog of the given project
+#        """
 
-       #        else:
-       #               try:
-       #                      git_log = target.get_reflog()
-       #                      return git_log
-              
-       #               except Exception:
-       #                      print('Error retrieving reflog for {}'.format(project))
-       #                      raise LogRetrieveException                     
+#        repo=client.projects.get(project)
 
-              
-       # def parse_log(log, verbose=False):
-       #        """Parse the git reflog output of the given project and 
-       #        report the frequency of its commits
-       #        """
-                            
-       #        dates=[]
-       #        for commit in log:              
-       #               dates.append(commit['who']['date'].split()[0])
+#        try:
+#               target=repo.branches.get('refs/heads/'+branch)
 
-       #        unique_dates = set(dates)
-       #        freq = []
-       #        for dd in sorted(unique_dates, reverse=True):
-       #               occurances = Counter(dates)[dd]
-       #               freq.append(occurances)
-       #               if verbose:
-       #                      print('{} commits issued on {}.'.format(occurances, dd))
-                     
-       #        min_freq = min(freq)
-       #        max_freq = max(freq)
-       #        avg_freq = sum(freq)/len(freq)
+#        except Exception:
+#               print('{}'.format(project))
 
-       #        return min_freq, max_freq, avg_freq    
+#        else:
+#               try:
+#                      git_log = target.get_reflog()
+#                      return git_log
+
+#               except Exception:
+#                      print('Error retrieving reflog for {}'.format(project))
+#                      raise LogRetrieveException
+
+
+# def parse_log(log, verbose=False):
+#        """Parse the git reflog output of the given project and
+#        report the frequency of its commits
+#        """
+
+#        dates=[]
+#        for commit in log:
+#               dates.append(commit['who']['date'].split()[0])
+
+#        unique_dates = set(dates)
+#        freq = []
+#        for dd in sorted(unique_dates, reverse=True):
+#               occurances = Counter(dates)[dd]
+#               freq.append(occurances)
+#               if verbose:
+#                      print('{} commits issued on {}.'.format(occurances, dd))
+
+#        min_freq = min(freq)
+#        max_freq = max(freq)
+#        avg_freq = sum(freq)/len(freq)
+
+#        return min_freq, max_freq, avg_freq
